9387707fc8c6c6d99c4b31d7a79d219c7d254c27
DESKTOP-GH4LTIO\hi <gps95wwkhhev@gmail.com>
2024-02-20 18:38:25 +0900

    [Feat] 조회 및 수정/삭제 기능 구현1

--- src/test/java/org/ohgiraffers/mission02board/controller/PostControllerTest.java
+++ src/test/java/org/ohgiraffers/mission02board/controller/PostControllerTest.java
@@ -1,2 +1,181 @@
-package org.ohgiraffers.mission02board.controller;public class PostController {
+package org.ohgiraffers.mission02board.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.ohgiraffers.mission02board.dto.*;
+import org.ohgiraffers.mission02board.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/* 통합테스트 & 단위테스트
+*   통합테스트 :
+*   모듈을 통합하는 과정에서 모듈간의 호환성을 확인하기 위해 수행되는 테스트
+*   통합테스트는 API 의 모든 과정이 올바르게 동작하는지 확인하는 것
+*
+*   단위(유닛)테스트
+*   하나의 모듈을 기준으로 독립적으로 진행되는 가장 작은 단위의 테스트( 작은 단위 = 함수, 메소드
+*   단위테스트는 어플리케이션을 구성하는 하나의 기능이 올바르게 동작하는지 독립적으로 테스트하는 것
+*
+*   @WebMvcTst
+*   MVC 를 위한 테스트, 컨트롤러가 예상대로 동작하는지 테스트하는데 사용된다.
+*   웹 어플리케이션을 어플리케이션 서버에 배포하지 않고 테스트용 MVC 환경을 만들어 요청 및 전송 은답기능을 제공해준다.*/
+
+@WebMvcTest(PostController.class)
+public class PostControllerTest {
+
+    /* @Autowired
+    *   의존성 주입(DI)으르 할 떄, 사용하는 필요한 의존 객체의 타입이 해당하는 빈을 찾아 주입한다.
+    *   의존성 : 하나의 코드가 다른 코드에 의존하는 상태를 뜻한다.*/
+    @Autowired
+    MockMvc mockMvc;
+
+    /* @MockBean
+    *   스프링의 의존성 주입기능을 사용해 테스트 실행 시 실제 빈 대신에 Mockito 로 생성된
+    *   모의 객체를 스프링 어플리케이션 컨텍스트에 추가한다.*/
+    @MockBean
+    PostService postService;    //가짜 객체
+
+    /* ObjectMapper
+    *   특정 객체를 json 형태로 바꾸기 위해 사용한다.*/
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Test
+    @DisplayName("게시글 작성 기능 테스트")
+    void create_post_test() throws Exception {
+        //given
+        CreatePostRequest request = new CreatePostRequest("테스트 제목", "테스트 내용");
+        CreatePostResponse response = new CreatePostResponse(1L, "테스트 제목", "테스트 내용");
+
+        given(postService.createPost(any())).willReturn(response);
+
+        //when & then
+        mockMvc.perform(post("/api/v1/posts")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsBytes(request))
+        )
+                .andExpect(status(). isOk())
+                .andExpect(jsonPath("$.postId"). value(1L))
+                .andExpect(jsonPath("$.title"). value("테스트 제목"))
+                .andExpect(jsonPath("$.content").value("테스트 내용"))
+                .andDo(print());
+
+        //then
+    }
+
+    @Test
+    @DisplayName("게시글을 단일 조회하는 테스트")
+    void read_post_test() throws Exception {
+
+        //given
+        Long postId = 1L;
+
+        ReadPostResponse response = new ReadPostResponse(1L, "테스트 제목", "테스트 내용");
+
+        given(postService.readPostById(any())).willReturn(response);
+
+        //when & then
+        mockMvc.perform(get("/api/v1/posts/{postId}", postId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.postId").value(1L))
+                .andExpect(jsonPath("$.title").value("테스트 제목"))
+                .andExpect(jsonPath("$.content").value("테스트 내용"))
+                .andDo(print());
+        //then
+    }
+
+    @Test
+    @DisplayName("게시글을 업데이트하는 테스트")
+    void update_post_test() throws Exception {
+
+        //given
+        Long postId = 1L;
+
+        UpdatePostRequest request = new UpdatePostRequest("변경 제목", "변경 내용");
+        UpdatePostResponse response = new UpdatePostResponse(1L, "변경 제목", "변경 내용");
+
+        given(postService.updatePost(any(Long.class), any(UpdatePostRequest.class))).willReturn(response);
+
+        //when & then
+        mockMvc.perform(put("/api/v1/posts/{postId}", postId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(request))
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.postId").value(1L))
+                .andExpect(jsonPath("$.title").value("변경 제목"))
+                .andExpect(jsonPath("$.content").value("변경 내용"))
+                .andDo(print());
+        //then
+    }
+
+    @Test
+    @DisplayName("게시글을 삭제하는 테스트")
+    void delete_post_test() throws Exception{
+
+        //given
+        Long postId = 1L;
+
+        DeletePostResponse response = new DeletePostResponse(1L, "테스트 제목", "테스트 내용");
+
+        given(postService.deletePost(any())).willReturn(response);
+
+
+        //when & then
+
+        mockMvc.perform(delete("/api/v1/posts/{postId}", postId))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.postId").value(1L))
+//                .andExpect(jsonPath("$.title").value("테스트 제목"))
+//                .andExpect(jsonPath("$.content").value("테스트 내용"))
+                .andDo(print());
+    }
+
+
+    @Test
+    @DisplayName("게시글 전체조회 하는 기능")
+    void readAll_post_test() throws Exception {
+
+        //given
+        int page = 0;
+        int size = 5;
+        PageRequest pageRequest = PageRequest.of(page, size);
+        ReadPostResponse readPostResponse = new ReadPostResponse(1L, "테스트 제목", "테스트 내용");
+        List<ReadPostResponse> responses = new ArrayList<>();
+        responses.add(readPostResponse);
+
+        Page<ReadPostResponse> pageResponses = new PageImpl<>(responses, pageRequest, responses.size());
+
+        given(postService.readAllPost(any())).willReturn(pageResponses);
+
+        //when & then
+        mockMvc.perform(get("/api/v1/posts"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.content[0].postId").value(readPostResponse.getPostId()))
+                .andExpect(jsonPath("$.content[0].title").value(readPostResponse.getTitle()))
+                .andExpect(jsonPath("$.content[0].content").value(readPostResponse.getContent()))
+                .andDo(print());
+
+    }
+
 }
--- src/test/java/org/ohgiraffers/mission02board/service/PostServiceTest.java
+++ src/test/java/org/ohgiraffers/mission02board/service/PostServiceTest.java
@@ -1,2 +1,163 @@
-package org.ohgiraffers.mission02board.service;public class PostServiceTest {
+package org.ohgiraffers.mission02board.service;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.ohgiraffers.mission02board.domain.Post;
+import org.ohgiraffers.mission02board.dto.*;
+import org.ohgiraffers.mission02board.repository.PostRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.when;
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+public class PostServiceTest {
+
+    /* @Mock
+    *   가짜 객체, 테스트 실행 시 실제가 아닌 Mock 객체를 반환한다.*/
+    @Mock
+    private PostRepository postRepository;  //가짜 객체
+
+    /* @InjectionMocks
+    *   Mock 객체가 주입 될 클래스를 지정한다.*/
+    @InjectMocks
+    private PostService postService;
+
+    private Post post;
+
+    private Post savedPost;
+
+    private CreatePostRequest createPostRequest;
+
+    private UpdatePostRequest updatePostRequest;
+
+    @BeforeEach
+    void setUp() {
+        //초기화
+        post = new Post(1L, "테스트 제목", "테스트 내용");
+        savedPost = new Post(2L, "저장되어 있던 테스트 제목", "저장되어 있던 테스트 내용");
+        createPostRequest = new CreatePostRequest("테스트 제목", "테스트 내용");
+        updatePostRequest = new UpdatePostRequest("변경된 테스트 제목", "변경된 테스트 내용");
+    }
+
+    @Test
+    @DisplayName("게시글 작성 기능 테스트")
+    void create_post_test() {
+        //given
+
+        //mockito 기본 형태
+        //when(postRepository.save(any())).thenReturn(post);
+
+        //BDDMockito 형태
+        given(postRepository.save(any())).willReturn(post);
+
+        //when
+        CreatePostResponse createPostResponse = postService.createPost(createPostRequest);
+
+        //then
+        assertThat(createPostResponse.getPostId()).isEqualTo(1L);
+        assertThat(createPostResponse.getTitle()).isEqualTo("테스트 제목");
+        assertThat(createPostResponse.getContent()).isEqualTo("테스트 내용");
+    }
+
+    @Test
+    @DisplayName("postId로 게시글을 조회하는 기능 테스트")
+    void read_post_test_1() {
+        //given
+        when(postRepository.findById(any())).thenReturn(Optional.of(savedPost));
+
+        //when
+        ReadPostResponse readPostResponse = postService.readPostById(savedPost.getPostId());
+
+        //then
+        assertThat(readPostResponse.getPostId()).isEqualTo(savedPost.getPostId());
+        assertThat(readPostResponse.getTitle()).isEqualTo(savedPost.getTitle());
+        assertThat(readPostResponse.getContent()).isEqualTo(savedPost.getContent());
+    }
+
+    @Test
+    @DisplayName("postId로 게시글을 찾지 못했을 때, 지정한 Exception 을 발생시켰는지 테스트")
+    void read_post_test_2() {
+        //given
+        given(postRepository.findById(any())).willReturn(Optional.empty());
+
+        //when & then
+        assertThrows(EntityNotFoundException.class, () ->
+                postService.readPostById(2L));
+
+        //then
+    }
+
+    @Test
+    @DisplayName("전체 게시글 조회 기능 테스트")
+    void read_post_test() {
+        //given
+        Pageable pageable = PageRequest.of(0, 5);
+        List<Post> posts = Arrays.asList(post, savedPost);
+        Page<Post> postPage = new PageImpl<>(posts, pageable, posts.size());
+
+        given(postRepository.findAll(pageable)).willReturn(postPage);
+
+        //when
+        Page<ReadPostResponse> responses = postService.readAllPost(pageable);
+
+        //then
+        assertThat(responses.getContent()).hasSize(2);
+        assertThat(responses.getContent().get(0).getTitle()).isEqualTo("테스트 제목");
+        assertThat(responses.getContent().get(0).getContent()).isEqualTo("테스트 내용");
+        assertThat(responses.getContent().get(1).getTitle()).isEqualTo("저장되어 있던 테스트 제목");
+        assertThat(responses.getContent().get(1).getContent()).isEqualTo("저장되어 있던 테스트 내용");
+    }
+
+    @Test
+    @DisplayName("postId로 게시글 수정 기능 테스트")
+    void update_post_test () {
+
+        //given
+        given(postRepository.findById(any())).willReturn(Optional.of(savedPost));
+
+        //given(postRepository.save(any())).willReturn(Optional.of(updatePostRequest));
+
+
+        //when
+        UpdatePostResponse response = postService.updatePost(savedPost.getPostId(), updatePostRequest);
+
+        //then
+        assertThat(response.getPostId()).isEqualTo(savedPost.getPostId());
+        assertThat(response.getTitle()).isEqualTo("변경된 테스트 제목");
+        assertThat(response.getContent()).isEqualTo("변경된 테스트 내용");
+
+    }
+
+    @Test
+    @DisplayName("postId로 게시글 삭제 기능 테스트")
+    void delete_post_test () {
+
+        //given
+        given(postRepository.findById(any())).willReturn(Optional.of(savedPost));
+
+
+        //when
+        DeletePostResponse response = postService.deletePost(savedPost.getPostId());
+
+        //then
+        assertThat(response.getPostId()).isEqualTo(2L);
+    }
+
 }
